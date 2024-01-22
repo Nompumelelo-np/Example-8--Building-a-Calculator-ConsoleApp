38a933e1db56b8a2cec1a974d86075e1d6a31f38
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-22 18:02:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_8_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //to convert a string to a number
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6);
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+            //can also convert to double
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            double num4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num3 + " + " + num4 + " = " + (num3 + num4));
+            Console.Read();
+        }
+    }
+}
